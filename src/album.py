8018bd3465997d9dda32dd6e324b4acb272c7f4e
Sergiusz Nowicki <sergiusz.nowicki@pm.me>
2024-02-29 09:54:34 +0100

    .

--- src/album.py
+++ src/album.py
@@ -7,7 +7,7 @@ class Album:
     def __init__(self, album_path):
         print(f'Loading album {album_path}')
         metadata_json_file = JsonFile(os.path.join(album_path, '.metadata'))
-        entries = (AlbumEntry(path, metadata_json_file) for path in os.listdir(album_path) if os.path.isfile(path))
+        entries = (AlbumEntry(os.path.join(album_path, path) , metadata_json_file) for path in os.listdir(album_path) if os.path.isfile(path))
         self.entries = (e for e in entries if not e.is_generic())
 
 
